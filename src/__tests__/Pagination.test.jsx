0cc6ea7485c0a6b6dadfd62a739e14ec99a8130c
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-07-04 12:16:18 +0300

    testing if no movies exist, the pagination component dosen't render

--- src/__tests__/Pagination.test.jsx
+++ src/__tests__/Pagination.test.jsx
@@ -1,6 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, vi } from "vitest";
 import PaginationComponent from "../components/Pagination";
+import { renderWithRedux } from "./utils/ReduxConfig";
+import App from "../App";
+import { createMSW } from "./utils/MSWConfig";
+
+createMSW([
+  {
+    method: "get",
+    url: "https://api.themoviedb.org/3/movie/popular",
+    response: {
+      results: [],
+      total_pages: 1,
+    },
+  },
+]);
 
 describe("testing pagination component", () => {
   test("testing pagination ui", async () => {
@@ -8,7 +22,12 @@ describe("testing pagination component", () => {
     const pagesCount = 10;
     render(<PaginationComponent getPage={getPage} pageCount={pagesCount} />);
     const buttons = await screen.findAllByRole("button");
-    console.log(buttons.length);
     expect(buttons).toHaveLength(7);
+  });
+
+  test("testing if no movies, no pagination", async () => {
+    renderWithRedux(<App />);
+    const buttons = screen.queryAllByRole("button");
+    expect(buttons).toHaveLength(0);
   });
 });
